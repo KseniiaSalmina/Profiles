04310f764ab6d9b1b0468388892e77787b5ff3ca
KseniiaSalmina <sal.kseniia@gmail.com>
2024-05-09 01:08:44 +0400

    add authorization

---
+++ internal/api/authorization.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/KseniiaSalmina/Profiles/internal/api/validation"
+)
+
+func (s *Server) authorization(r *http.Request) (bool, error) {
+	authString, ok := r.Header["Authorization"]
+	if !ok {
+		return false, validation.ErrIncorrectAuth
+	}
+
+	username, password, err := validation.AuthString(authString[0])
+	if err != nil {
+		return false, err
+	}
+
+	user, err := s.storage.GetUserByUsername(username)
+	if err != nil {
+		return false, err
+	}
+
+	if err := validation.User(username, password, *user); err != nil {
+		return false, err
+	}
+
+	return user.Admin, nil
+}
